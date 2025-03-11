5a4a8e0f0ef8fdde9473d8018e68bdf6016223de
DeepikaSaravanan5 <deepika008@e.ntu.edu.sg>
2025-03-11 22:19:45 +0800

    added post api
    no idea why it states this many files changed

--- backend/api/chatbot/views.py
+++ backend/api/chatbot/views.py
@@ -11,6 +11,7 @@ from django.http import JsonResponse
 def index(request):
     return JsonResponse({"chatbot okay"})
 
+
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 #retieve all chat sessions obtained w a certain user
@@ -20,6 +21,14 @@ def get_chat_session(request):
     serializer=ChatSessionSerializer(history, many=True)
     return Response(serializer.data)
 
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+#store a new chat session
+def create_chat_session(request):
+    user = request.user
+    session = ChatSession.objects.create(user=user)
+    serializer = ChatSessionSerializer(session)
+    return Response(serializer.data)
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
@@ -29,6 +38,17 @@ def get_chat_message(request,session_id):
     session = get_object_or_404(ChatSession, session_id=session_id, user=user)
     messages=ChatMessage.objects.filter(session=session)
     serializer=ChatMessageSerializer(messages, many=True)
+    return Response(serializer.data)
+
+@api_view(["POST"]) 
+@permission_classes([IsAuthenticated]) 
+def create_chat_message(request, session_id): 
+    user = request.user 
+    sender = request.data.get("sender")
+    session = get_object_or_404(ChatSession, session_id=session_id, user=user) 
+    content_of_message = request.data.get("message") 
+    message = ChatMessage.objects.create(session=session, sender=sender,message=content_of_message) 
+    serializer = ChatMessageSerializer(message) 
     return Response(serializer.data)
 
 
